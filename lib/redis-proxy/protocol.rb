b19b405bc4bb0653b59ab036fd742a710b9f5ec8
Jeong, Jiung <ethernuiel@sanultari.com>
2013-05-08 12:04:37 +0900

    refactoring protocol parser

--- lib/redis-proxy/protocol.rb
+++ lib/redis-proxy/protocol.rb
@@ -2,27 +2,61 @@ module RedisProtocol
   OP_DELIMITER = "\r\n"
 
   class << self
-    def parse_request data
+    def parse_request(data)
+      type = check_request_type data
+
+      RedisProtocol.send type, data
+    end
+
+    def recognize(data)
+      case check_request_type data
+        when :inline
+          data.split[0]
+        else
+          data.split(OP_DELIMITER)[2]
+      end
+    end
+
+    def check_request_type(data)
+      delimiter_count = data.count OP_DELIMITER[0]
+      case delimiter_count
+        when delimiter_count > 1
+          :standard
+        else
+          :inline
+      end
+    end
+
+    def standard(data)
       components = []
-      operand_length, payload = next_token data
-      raise 'invalid packet' unless operand_length.start_with? '*'
-      operand_length = operand_length[1..-1].to_i
+      operand_length, payload = operand_length_for data
       1.upto(operand_length).each do |_|
-        field_length, payload = next_token payload
-        raise 'invalid length format' unless field_length.start_with? '$'
-        field_length = field_length[1..-1].to_i
-        data, payload = next_token payload, field_length
+        data, payload = unpack_payload(payload)
         components << data
       end
-
       components
     end
 
-    def recognize data
-      data.split(OP_DELIMITER)[2]
+    def operand_length_for(data)
+      operand_length, payload = next_token data
+      raise 'invalid packet' unless operand_length.start_with? '*'
+      operand_length = operand_length[1..-1].to_i
+
+      [operand_length, payload]
     end
 
-    def next_token data, length = 0
+    def unpack_payload(payload)
+      field_length, payload = next_token payload
+      raise 'invalid length format' unless field_length.start_with? '$'
+      field_length = field_length[1..-1].to_i
+      next_token payload, field_length
+    end
+
+    def inline(data)
+      data.split
+    end
+
+    def next_token(data, length = 0)
       index = length
       index = data.index OP_DELIMITER if index == 0
       current_data = data[0...index]
@@ -31,5 +65,7 @@ module RedisProtocol
 
       [current_data, next_data]
     end
+
+    private :next_token, :standard, :inline, :operand_length_for, :unpack_payload
   end
 end
